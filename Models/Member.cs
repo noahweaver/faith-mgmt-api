75c9e1b5be6244d54ba80d251acb4768b112545d
noah weaver <noahweaver@aol.com>
2024-03-20 15:51:29 -0500

    Adding member and request classes

---
+++ Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FaithMgmtAPI.Models
+{
+    public class Member(string name)
+    {
+        [Key]
+        public int Id { get; }
+
+        public string Name { get; set; } = name;
+        public DateTime DateAdded { get; }
+        public List<int>? Requests { get; set; }
+    }
+}
---
+++ Models/Request.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FaithMgmtAPI.Models
+{
+    public class Request(int id, string prayerRequest, int? memberId = null, string? name = null, DateTime? createdAt = null)
+    {
+        [Key]
+        public int Id { get; } = id;
+        
+        public int? MemberId { get; set; } = memberId;
+
+        public string? Name { get; set; } = name;
+        public string PrayerRequest { get; set; } = prayerRequest;
+
+        public DateTime? CreatedAt { get; } = createdAt;
+
+    }
+}
